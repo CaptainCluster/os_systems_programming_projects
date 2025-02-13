2567a6a59d17bdbe2dd1112856734757550bce9b
CaptainCluster <sranta.ville@gmail.com>
2025-02-13 13:03:40 +0200

    Linked list works

--- project1/README.md
+++ project1/README.md
@@ -2,3 +2,4 @@
 Sources
 ---
 [Linked list data structure](https://www.cprogramming.com/tutorial/c/lesson15.html)
+[Using getline() function](https://c-for-dummies.com/blog/?p=1112)
--- project1/reverse.c
+++ project1/reverse.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
+ * OS and Systems Programming course - Project 1
  * The sources used for this repository are in the README.md file
  */ 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define ERR_INPUTFILE  "Error when attempting to open the input file!\n"
 #define ERR_OUTPUTFILE "Error when attempting to open the output file!\n"
+#define ERR_MALLOC     "Unable to allocate memory.\n"
+#define ERR_MEMORY     "Out of memory!\n"
+#define BUFFER_SIZE    256
 
 struct node {
   char* line;
@@ -38,6 +43,8 @@ int main(int argc, char** argv)
   FILE* outputFile;
   struct node *root;
   struct node *conductor;
+  char* buffer;
+  size_t bufferSize = BUFFER_SIZE;
 
   /** 
    * Defining the input and output files for the program to operate with
@@ -68,9 +75,46 @@ int main(int argc, char** argv)
       exit(0);
   }
 
-  // Continuing with the defined files
-  
-  
+  // Allocating memory for both the root of the linked list and the next node
+  root = malloc(sizeof(struct node));
+  root->next = malloc(sizeof(struct node));
+  conductor = root;
+
+  if (conductor == 0)
+  {
+    fprintf(stderr, "%s", ERR_MEMORY);
+    exit(1);
+  }
+
+  // Putting the input lines into a linked list
+  while (1)
+  {
+    if ((buffer = (char*) malloc(BUFFER_SIZE * sizeof(char))) == NULL)
+    {
+      fprintf(stderr, "%s\n", ERR_MALLOC);
+      exit(1);
+    }
+    if(getline(&buffer, &bufferSize, inputFile) <= 0)
+    {
+      break;
+    }
+    conductor->line = strdup(buffer);
+    conductor->next = malloc(sizeof(struct node));
+    conductor = conductor->next;
+  }
+
+  conductor->next = malloc(sizeof(struct node));
+  conductor = conductor->next;
+  conductor->next = 0;
+
+  // Going through the linked list one node at a time
+  conductor = root;
+  while ( conductor->next->next != 0 ) 
+  {
+    fprintf(outputFile, "%s", conductor->line );
+    conductor = conductor->next;
+  }
+
   fclose(inputFile);
   fclose(outputFile);
   return 0;
